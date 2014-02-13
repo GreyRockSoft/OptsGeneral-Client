b0a41dc4dce3589cda843f80e09ef36bf3985d85
Ryan Moore <ryan.moore@greyrocksoft.com>
2014-02-13 15:34:16 -0700

    Added in file download, tar support, and working on loading in a file to execute a general script.

--- bin/optsGeneral.js
+++ bin/optsGeneral.js
@@ -5,73 +5,26 @@ if(process.getuid() !== 0) {
   console.warn('Warning: You do not have sudo access.  Some system modification may not perform correctly.');
 }
 
+function loadGeneralFile(fileName) {
+  try {
+    return require(fileName);
+  }
+  catch (e) {
+    console.log('Could not load: ' + fileName);
+    return undefined;
+  }
+}
+
+var fs = require('fs');
 var optsGeneral = require('../');
 
-optsGeneral.package.update({},function(err) {
-  if(err) {
-    console.error(err);
-  }
-  else {
-    console.log('Installed sl');
-  }
-});
-/*
-optsGeneral.package.install('sl',{},  function(err) {
-  if(err) {
-    console.error(err);
-  }
-  else {
-    console.log('Installed sl');
-  }
-});
+//search for the genearl.js file and execute it
 
-/*
-optsGeneral.service.service('nginx', 'status', function(err) {
-  if(err) {
-    console.error(err);
-  }
-  else {
-    console.log('Started nginx.');
-  }
-});
-
-
-optsGeneral.info.serverInfo(function (err, result){
-  if(err) {
-    console.log("Failed to get system details.");
-  }
-  console.log(JSON.stringify(result, null, 2));
-});
-
-
-
-optsGeneral.users.addUser({name: 'test', password:'test'}, function(err) {
-  if(err) {
-    console.log('Failed to create user');
-  }
-  else {
-    console.log('Created user Test');
-    optsGeneral.file.writeFile('./test', 'This is a test', {perm: 0755, owner: 'test', group: 'test'}, function(err) {
-      if(err) {
-        console.error(err);
-      }
-      else {
-        console.log('It worked!');
-      }
-    });
-  }
-});
-
-
-optsGeneral.template.template('template.tmpl', 'rendered', {myValue: 'Yay!'}, {owner: 'test'}, function(err) {
-  if(err) {
-    console.log('Failed to create the template:' + err.toString());
-  }
-  else {
-    console.log('Created the template');
-  }
-});
- */
-
-
+var general = require('./general.js');
+if(general) {
+  general(optsGeneral);
+}
+else {
+  console.error('Could not find general.js file.');
+}
 
